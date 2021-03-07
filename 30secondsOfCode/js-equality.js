e5cb2a229cfe1aa9a07760c0275c63af6d7f0e8d
igaozp <igaozp@hotmail.com>
2021-03-07 20:53:35 +0800

    [Feat] 30 seconds of code:  js equality operators example.

---
+++ 30secondsOfCode/js-equality.js
@@ -0,0 +1,30 @@
+const num = 0;
+const str = '0';
+const obj = new String(0);
+const bool = false;
+const undef = undefined;
+const nil = null;
+
+console.dir([
+    num == str,     // 0 == 0, true
+    num == bool,    // 0 == 0, true
+    str == obj,     // '0' == '0', true
+    obj == num,     // 0 == 0, true
+    bool == str,    // 0 == 0, true
+    bool == obj,    // 0 == 0, true
+    bool == nil,    // false
+    undef == nil,   // true
+    undef == bool   // false
+]);
+
+console.dir([
+    num === str,    // types don't match, false
+    num === bool,   // types don't match, false
+    str === obj,    // types don't match, false
+    obj === num,    // types don't match, false
+    bool === str,   // types don't match, false
+    bool === obj,   // types don't match, false
+    bool === nil,   // types don't match, false
+    undef === nil,  // types don't match, false
+    undef === bool  // types don't match, false
+])
